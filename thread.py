7e2acb2d1b08cdbb9e6092707179e5fb6d97740d
Osgood <oushigang19@mails.ucas.ac.cn>
2024-02-28 13:55:19 +0800

    remove the key;

--- thread.py
+++ thread.py
@@ -15,7 +15,7 @@ load_dotenv()
 
 required_version = version.parse("1.1.1")
 current_version = version.parse(openai.__version__)
-# OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 if current_version < required_version:
   raise ValueError(
       f"Error: OpenAI version {openai.__version__} is less than the required version 1.1.1"
